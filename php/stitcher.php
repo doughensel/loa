b6d524117d6b6238ca3e84071669122da129bf3d
Hensel <douglas.hensel@gmail.com>
2016-09-07 08:43:38 -0400

    rudimentary php script to construct the index.html page

--- php/stitcher.php
+++ php/stitcher.php
@@ -1,5 +1,44 @@
 <?php
 
+$reference = '../js/json/modules.json';
+$json = array();
+$output = '';
+$fName = '';
+$fExists = false;
 
-header( 'Location: ../amp-demo.html' );
+if( file_exists( $reference ) ){
+	$json = json_decode(file_get_contents($reference), true) ?: array();
+}else{
+	file_put_contents( $reference, '' );
+}
+
+foreach( $json[module] as $key => $value ){
+	if( $value[active] ){
+
+		$fExists = false;
+		$fName = '../modules/' . $value[name] . '.html';
+
+
+		if( !file_exists( $fName ) ){
+			$fName = '../templates/' . $value[name] . '.html';
+			if( file_exists( $fName ) ){
+				$fExists = true;
+			}
+		}else{
+			$fExists = true;
+		}
+
+		if( $fExists ){
+		 	$html .= file_get_contents($fName);
+		}
+		
+	}
+}
+
+
+$template = file_get_contents('../templates/index.html');
+$output = str_replace( '{$output}', $html, $template );
+file_put_contents('../index.html', $output);
+
+header( 'Location: ../index.html' );
 ?>
