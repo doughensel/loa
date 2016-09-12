6b0d1d877e2f62d915b3d85602a322b2b756ece1
Hensel <douglas.hensel@gmail.com>
2016-09-12 15:51:30 -0400

    structure updates

--- php/inspiration.php
+++ php/inspiration.php
@@ -21,19 +21,21 @@ $result = array( "desc" => $desc, "ref" => $ref, "refAdd" => $refAdd, "date" => 
 if( $id >= 0 ){
 	$json[$id] = $result;
 }else{
-	array_push( $json, $result );
+	if( $desc !== '' ){
+		array_push( $json, $result );
+	}
 }
 
 file_put_contents($reference, json_encode($json));
 
-$html = '<ul>';
+$html = '<ul id="inspirations">';
 $temp = array();
 $random = 0;
 $max  = ( count($json) < 5 ? count($json) : 5 );
 for( $x = 0; $x<$max; $x++ ){
 	$random = rand(0, ( count($json) - 1) );
 	$temp   = array_splice( $json, $random, 1 );
-	$html  .= '<li><span class="txtDesc">' . $temp[0]["desc"] . '</span> <span class="txtRef">' . $temp[0]["ref"] . ' ' . $temp[0]["refAdd"] . '</span> <span class="txtDate">' . $temp[0]["date"] . '</span></li>';
+	$html  .= '<li><a href="inspiration.html?id=' . $x . '"><span class="txtDesc">' . $temp[0]["desc"] . '</span> <span class="txtRef">' . $temp[0]["ref"] . ' ' . $temp[0]["refAdd"] . '</span> <span class="txtDate">' . $temp[0]["date"] . '</span></a></li>';
 }
 $html .= '</ul>';
 
